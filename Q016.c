6fb3bfecfaecbeefa07a06b239c5d37b1fa4dab7
GOKUL G <gokulggopi2007@gmail.com>
2026-07-16 08:48:30 +0530

    Add files via upload

---
+++ Q016.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
+                printf("YES");
+                return 0;
+            }
+        }
+    }
+    printf("NO");
+    return 0;
+}
