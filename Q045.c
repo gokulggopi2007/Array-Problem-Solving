ea4024c108f41ae10e1a3eb5f85b850d8ae4a6c7
GOKUL G <gokulggopi2007@gmail.com>
2026-08-20 23:07:45 +0530

    Add files via upload

---
+++ Q045.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int co=0;
+    for(int i=0;i<n;i++){
+        int count=0,num=arr[i];
+        while(num!=0){
+            count++;
+            num/=10;
+        }
+        if(count%2==0){
+            co++;
+        }
+    }
+    printf("%d",co);
+    return 0;
+}
